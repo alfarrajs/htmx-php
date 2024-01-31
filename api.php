bbb701390a8e7b5ac85d95a017b28c5f1a7875cc
Izzedeen <rashed@Izzedeens-MacBook-Pro.local>
2024-01-31 14:19:15 +0200

    test

---
+++ api.php
@@ -0,0 +1,49 @@
+<?php
+
+$connection = mysqli_connect("localhost", "root", "", "htmx");
+$post_title = $_POST['title'];
+$post_body = $_POST['body'];
+
+function render($title, $body)
+{
+    echo "
+        <div class='card mb-2 nh-post ' style='width: 18rem;'>
+        <div class='card-body'>
+        <h5 class='card-title'>$title</h5>
+        <p class='card-text'>$body</p>
+    </div>
+    
+    <div class='card-body'>
+        <a href='#' class='card-link bg-danger text-light text-decoration-none p-2 rounded'>delete</a>
+        <a href='#' class='card-link bg-success text-light text-decoration-none p-2 rounded'>update</a>
+    </div>
+        </div>";
+}
+
+switch ($_GET['action']) {
+    case 'add_post':
+        render($post_title, $post_body);
+        $query = "INSERT INTO posts(title, body) VALUES('$post_title', '$post_body')";
+        mysqli_query($connection, $query);
+        break;
+    case 'get_post':
+        $query = "SELECT * FROM posts";
+        $result = mysqli_query($connection, $query);
+        while ($row = mysqli_fetch_assoc($result)) {
+            echo "
+            <div class='card mb-2 nh-post ' style='width: 18rem;'>
+            <div class='card-body'>
+            <h5 class='card-title'>$row[title]</h5>
+            <p class='card-text'>$row[body]</p>
+        </div>
+        
+        <div class='card-body'>
+            <a href='#' class='card-link bg-danger text-light text-decoration-none p-2 rounded'>delete</a>
+            <a href='#' class='card-link bg-success text-light text-decoration-none p-2 rounded'>update</a>
+        </div>
+            </div>";
+        }
+        break;
+}
+
+?>
